67e64407adfe6408c7d7730cf4f984301ccd595e
Poulette04 <alia.serrano@duocuc.cl>
2025-06-17 09:34:13 -0400

    testing
    creacion de test

---
+++ src/test/java/com/fullstack/perfulandiaSPA/Controller/carritoControllerIntegrationTest.java
@@ -0,0 +1,21 @@
+package com.fullstack.perfulandiaSPA.Controller;
+import com.fullstack.perfulandiaSPA.Model.;
+import com.full.bibliotecaduocV2.Service.LibroService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(carritoController.class)
+public class carritoControllerIntegrationTest {
+    
+}
---
+++ src/test/java/com/fullstack/perfulandiaSPA/Controller/perfumeControllerIntegrationTest.java
@@ -0,0 +1,89 @@
+package com.fullstack.perfulandiaSPA.Controller;
+import com.fullstack.perfulandiaSPA.Model.Perfumes;
+import com.fullstack.perfulandiaSPA.Service.perfumeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(perfumeController.class)
+public class perfumeControllerIntegrationTest {
+     @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private perfumeController perfumeService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void listarPerfumes_debeRetornarListaJson() throws Exception {
+        List<Perfumes> perfumes = List.of(
+                new Perfumes(1, "vip 212", "Carolina Herrera", 89990, 20,"Fragancia fresca",20),
+                new Perfumes(2, "Perfume 2", "desconocido", 50000, 20, "neuva fragancia", 10)
+        );
+
+        when(perfumeService.getPerfume()).thenReturn(perfumes);
+
+        mockMvc.perform(get("/api/v1/perfumes"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].nombrePerfume").value("vip 212"));
+    }
+
+    @Test
+    void agregarPerfumes_debeGuardarYRetornarPerfumes() throws Exception {
+        Perfumes perfumes = new Perfumes(1, "perfume1", "carolina herrera", 59990, 100, "Nueva fragancia", 12);
+
+        when(perfumeService.savePerfume(any(Perfumes.class))).thenReturn(perfumes);
+
+        mockMvc.perform(post("/api/v1/perfumes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(perfumes)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombrePerfume").value("vip 212"));
+    }
+
+    @Test
+    void buscarPerfume_porId_existente() throws Exception {
+        Perfumes perfumes = new Perfumes(1, "vip 212", "carolina herrera", 89990, 200, "fragancia fresca", 20);
+
+        when(perfumeService.getPerfumes(5)).thenReturn(perfumes);
+
+        mockMvc.perform(get("/api/v1/perfumes/5"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombrePerfumes").value("Buscado"));
+    }
+
+    @Test
+    void eliminarLibro_existente() throws Exception {
+        when(libroService.deleteLibro(3)).thenReturn("producto eliminado");
+
+        mockMvc.perform(delete("/api/v1/libros/3"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("producto eliminado"));
+    }
+
+    @Test
+    void totalLibrosV2_debeRetornarCantidad() throws Exception {
+        when(libroService.totalLibrosV2()).thenReturn(10);
+
+        mockMvc.perform(get("/api/v1/libros/total"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("10"));
+    }
+
+}
