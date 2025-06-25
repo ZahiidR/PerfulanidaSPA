c64a8b636d50189d532f27ba3d8df955b796cb85
ZahiidR <za.rivera@duocuc.cl>
2025-06-25 01:33:59 -0400

    perfumeControllerV2 y propiedades swagger
    se implemento el controller v2 de perfume pero, tiene dos errores del methodOn

--- src/main/java/com/fullstack/perfulandiaSPA/Controller/perfumeControllerV2.java
+++ src/main/java/com/fullstack/perfulandiaSPA/Controller/perfumeControllerV2.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import java.util.List;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,49 +14,67 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.fullstack.perfulandiaSPA.Assemblers.perfumeModelAssembler;
+import com.fullstack.perfulandiaSPA.Controller.perfumeController;
 
-//Importar el assembler para HATEOAS de este controlador
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.MediaTypes;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v2/perfumes")
 public class perfumeControllerV2 {
+
     @Autowired
     private perfumeService perfumeserv;
 
-    @GetMapping
-    public List<Perfumes> listarPerfumes(){
-        return perfumeserv.getPerfumes();
+    @Autowired
+    private perfumeModelAssembler assembler;
+
+    @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public CollectionModel<EntityModel<Perfumes>> listarPerfumes() {
+        // Recoge la lista de perfumes y la convierte a EntityModel usando el assembler
+        List<EntityModel<Perfumes>> p = perfumeserv.getPerfumes().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        
+        // Aquí se genera un enlace al método listarPerfumes() usando methodOn
+        return CollectionModel.of(p, 
+            linkTo(methodOn(perfumeControllerV2.class).listarPerfumes()).withSelfRel());
     }
 
-    @PostMapping
-    public Perfumes agregarPerfume(@RequestBody Perfumes perfume){
-        return perfumeserv.savePerfume(perfume);
+    @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<Perfumes>> agregarPerfume(@RequestBody Perfumes perfume) {
+        Perfumes crear = perfumeserv.savePerfume(perfume);
+        return ResponseEntity.created(
+                linkTo(methodOn(perfumeControllerV2.class).buscarPerfumes(crear.getId())).toUri())
+                .body(assembler.toModel(crear));
     }
 
     @GetMapping("/{id}")
-    public Perfumes buscarPerfumes(@PathVariable int id){
+    public Perfumes buscarPerfumes(@PathVariable int id) {
         return perfumeserv.getPerfumeId(id);
     }
 
     @PutMapping("/{id}")
-    public Perfumes actualizaPerfumes(@PathVariable int id, @RequestBody Perfumes perfume){
+    public Perfumes actualizaPerfumes(@PathVariable int id, @RequestBody Perfumes perfume) {
         return perfumeserv.updatePerfume(perfume);
     }
 
     @DeleteMapping("/{id}")
-    public String eliminarPerfume(@PathVariable int id){
+    public String eliminarPerfume(@PathVariable int id) {
         return perfumeserv.deletePerfume(id);
     }
-
-
+   
     @GetMapping("/total")
-    public int totalPerfumesv2(){
+    public int totalPerfumesv2() {
         return perfumeserv.totalPerfumesv2();
     }
 }
-
-
-
-
-    
-
